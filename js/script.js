8595e50dc5a1a69f2b81601beaeb8c42154ad05d
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2020-07-10 21:51:03 -0300

    calculating individual IRA

--- js/script.js
+++ js/script.js
@@ -30,10 +30,16 @@ var checkbox = function(){
 var addGrade = function(e){
     e.preventDefault();
 
-    let grade = $("#nota").val();
-    let semester = $("#semestre").val();
-    let credits = $("#credito").val();
+    let grade = parseFloat($("#nota").val());
+    let semester = parseInt($("#semestre").val());
+    let credits = parseInt($("#credito").val());
+    
     let locked = document.getElementById("tranca").checked;
+    let miss =  document.getElementById("falta").checked;
+
+    if(miss){
+        grade = 0;
+    }
 
     let obj = {
         semester,
@@ -59,14 +65,39 @@ var renderTable = function(){
         tableContent.html("");
 
         grades.forEach(function(val){
-            tableContent.append(`<tr><td>${val.semester}º</td><td>${val.credits * 16}hrs</td><td>${ val.locked ? "TRANCADA" : val.grade}</td></tr>`);
+            tableContent.append(`<tr><td>${val.semester}º</td><td>${val.credits * 16}hrs</td><td>${ val.locked ? "<strong>TRANCADA</strong>" : val.grade}</td></tr>`);
         });
     }
 }
 
 var calculateIndividual = function(){
-    let t = 0;
 
+    console.log("testing");
+
+    let t = 0;
+    let c = 0;
+
+    let sigA = 0;
+    let sigB = 0;
+
+
+    grades.forEach(function(val){
+        if(val.locked){
+            t += val.credits * 16;
+        }else{
+            sigA += (Math.min(6,val.semester) * (val.credits * 16) * val.grade);
+            sigB += (Math.min(6,val.semester) * (val.credits * 16));
+
+        }
+
+        c += val.credits * 16;
+
+
+
+    });
+
+    let ans = (1 - (0.5*t)/c) * sigA/sigB;
+    console.log(ans);
 }
 
 checkbox("opcao");
